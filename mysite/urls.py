0c6c5ad479bd95e041bae86de369579fbb608b86
ubriela <ubriela@guest-wireless-upc-nat-206-117-88-096.usc.edu>
2014-01-20 14:14:54 -0800

    s

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    
     url(r'^polls/', include('polls.urls', namespace="polls")),
+    url(r'^books/', include('books.urls', namespace="books")),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^about/', TemplateView.as_view(template_name="about.html")),
 )
