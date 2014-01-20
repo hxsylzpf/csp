89636883ccd1c9507978d0b4426e883e355571b6
ubriela <ubriela@guest-wireless-upc-nat-206-117-88-096.usc.edu>
2014-01-20 00:03:17 -0800

    s

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -10,10 +10,19 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os, sys
 import site
 
+
+"""
 sys.path.append('D:\\home\\site\\wwwroot')
 sys.path.append('D:\\home\\site\\wwwroot\\mysite')
 sys.path.append('D:\\home\\site\\wwwroot\\polls')
+"""
 
+"""
+site.addsitedir('/opt/local/Library/Frameworks/Python.framework/Versions/2.6')
+sys.path.append('/Users/ubriela/git/GeoCrowdDemo/geocrowd/')
+sys.path.append('/Users/ubriela/git/GeoCrowdDemo/geocrowd/mysite/')
+sys.path.append('/Users/ubriela/git/GeoCrowdDemo/geocrowd/polls/')
+"""
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
