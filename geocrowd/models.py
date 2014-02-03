8a778d3ff777713239619ebfe367ee9c0ae8981d
ubriela <ubriela@192.168.0.15>
2014-02-03 02:43:44 -0800

    Using model form

--- geocrowd/models.py
+++ geocrowd/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
 # Create your models here.
+
+
+datasets_choices = (
+                ('mcdonald', 'McDonald'),
+                ('tiger', 'Tiger'),
+                ('shopping', 'Shopping'),
+                ('brightkite', 'Brightkite'),
+)
+algo_choices = (
+#                     ('baseline', 'Baseline'),
+#                     ('naive', 'Naive'),
+                ('greedy', 'Greedy'),
+                )
+ar_choices = (
+                ('linear', 'Linear'),
+                ('zipf', 'Zipf'),
+                ('const', 'Constant'),
+                ('step', 'Step'),
+                )
+budget_choices = (
+                ('0.1', '0.1'),
+                ('0.4', '0.4'),
+                ('0.7', '0.7'),
+                ('1.0', '1.0'),
+                )
+mar_choices = (
+                ('0.1', '0.1'),
+                ('0.4', '0.4'),
+                ('0.7', '0.7'),
+                ('1.0', '1.0'),
+                )
+mtd_choices = (
+                ('50', '50'),
+                ('100', '100'),
+                ('150', '150'),
+                ('200', '200'),
+                )
+u_choices = (
+                ('0', '0.4'),
+                ('1', '0.6'),
+                ('2', '0.8'),
+                ('3', '1.0'),
+                )
+
+class Geocrowd(models.Model):
+    datasets = models.CharField(max_length=10, null=False, blank=False, choices=datasets_choices)
+    algos = models.CharField(max_length=10, choices=algo_choices)
+    ars = models.CharField(max_length=10, choices=ar_choices)
+    budgets = models.CharField(max_length=10, choices=budget_choices)
+    mars = models.CharField(max_length=10, choices=mar_choices)
+    mtds = models.CharField(max_length=10, choices=mtd_choices)
+    us = models.CharField(max_length=10, choices=u_choices)
+    
+    def __unicode__(self):
+        return self.datasets
+    
+    
--- geocrowd/views.py
+++ geocrowd/views.py
@@ -1,13 +1,32 @@
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 from geocrowd.forms import GeocrowdForm
+from geocrowd.models import Geocrowd
         
         
 # Create your views here.
 def geocrowd(request):
     if request.method == 'POST':
-        form = GeocrowdForm(request.POST)
-        return HttpResponseRedirect('about.html')
+        # http request
+        a = Geocrowd.objects.get(pk=1)
+        form = GeocrowdForm(request.POST, instance=a)
+          
+        if form.is_valid():
+            # update database  
+            form.save(commit=True)
+            data = form.cleaned_data
+            url = "http://137.135.57.140/param/?"
+            url = url + "dataset=" + data['datasets']
+            url = url + "&eps=" + data['budgets']
+            url = url + "&algo=" + data['algos']
+            url = url + "&mar=" + data['mars']
+            url = url + "&arf=" + data['ars']
+            url = url + "&mtd=" + data['mtds']
+            url = url + "&utl=" + data['us']
+            url = url + "&rebuild=1"
+        
+            return HttpResponseRedirect(url)
     else:
-        form = GeocrowdForm()
+        a = Geocrowd.objects.get(pk=1)
+        form = GeocrowdForm(instance=a)
     return render(request, 'geocrowd_form.html', {'form':form})
